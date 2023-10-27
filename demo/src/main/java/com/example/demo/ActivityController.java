50401f33ee298c8594686e95ccbc6760737edb01
keeemhs <alenen0227@naver.com>
2023-10-27 20:43:10 +0900

    231027 commit

--- demo/src/main/java/com/example/demo/ActivityController.java
+++ demo/src/main/java/com/example/demo/ActivityController.java
@@ -1,2 +1,20 @@
-package com.example.demo;public class ActivityController {
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ActivityController {
+
+    @GetMapping("/Activity1")
+    public String checkAge(@RequestParam int age, Model model) {
+        if (age < 20) {
+            model.addAttribute("message", "미성년자입니다.");
+        } else {
+            model.addAttribute("message", "성인입니다.");
+        }
+        return "activity1";
+    }
 }
