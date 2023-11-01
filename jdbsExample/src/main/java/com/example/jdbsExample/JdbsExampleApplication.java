7fb3b5e92c06b2ec72773deca38f7c618f65c2a2
keeemhs <alenen0227@naver.com>
2023-11-01 15:34:29 +0900

    231101 commit

---
+++ jdbsExample/src/main/java/com/example/jdbsExample/JdbsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbsExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbsExampleApplication.class, args);
+	}
+
+}
---
+++ jdbsExample/src/test/java/com/example/jdbsExample/JdbsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbsExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
