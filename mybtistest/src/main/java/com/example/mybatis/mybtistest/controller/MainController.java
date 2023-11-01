e0f78b898af9f288abcd4502da1b29be30786195
keeemhs <alenen0227@naver.com>
2023-11-01 20:06:47 +0900

    231101 commit

--- mybtistest/src/main/java/com/example/mybatis/mybtistest/controller/MainController.java
+++ mybtistest/src/main/java/com/example/mybatis/mybtistest/controller/MainController.java
@@ -1,4 +1,22 @@
 package com.example.mybatis.mybtistest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+//import org.
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
+@Controller
 public class MainController {
+
+    @Autowired
+    private MainService mainService;
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        ArrayList<UserDTO> userList = (ArrayList<UserDTO>) mainService.getUserList();
+        model.addAttribute("list", userList);
+        return "user";
+    }
 }
