647be8e60716053d4cb58db89026c64fa026682b
keeemhs <alenen0227@naver.com>
2023-11-02 16:25:40 +0900

    231102 commit

---
+++ memberManage/src/test/java/com/example/member/memberManage/MemberManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.member.memberManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- mybtistest/src/main/java/com/example/mybatis/mybtistest/controller/MainController.java
+++ mybtistest/src/main/java/com/example/mybatis/mybtistest/controller/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -20,12 +20,12 @@ public class MainController {
 
     @GetMapping("/users")
     public String getUsers(Model model) {
-        ArrayList<UserDTO> userList = (ArrayList<UserDTO>) mainService.getUserList();
+        List<UserDTO> userList = mainService.getUserList();
         model.addAttribute("list", userList);
         return "user";
     }
 
-    @GetMapping("/user/insert")
+    @GetMapping("/insert")
     public String getInsertUser(@RequestParam String name, @RequestParam String address, Model model) {
 
 
